a5271aaea49be1549d534933b8724be0f1c324bf
David Factorial <david@recursivegarden.com>
2022-03-17 03:13:43 -0400

    Oops fix where every file becomes markdown type

--- lua/factorial/autocommands.lua
+++ lua/factorial/autocommands.lua
@@ -2,9 +2,11 @@ local group = vim.api.nvim_create_augroup("Factorial Autocmds", { clear = true }
 
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
     callback = function()
-        if vim.bo.filetype == '' then
-            vim.bo.filetype = 'markdown'
-        end
+        vim.schedule(function()
+            if vim.bo.filetype == '' then
+                vim.bo.filetype = 'markdown'
+            end
+        end)
     end,
     group = group,
 })
